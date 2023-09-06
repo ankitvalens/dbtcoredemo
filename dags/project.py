1da7a66522fbe1a099d94d9549e7bf72ddd3cd5b
ankit <ankit.devani@rackspace.com>
2023-09-06 11:32:13 +0530

    profile changes

--- dags/project.py
+++ dags/project.py
@@ -12,7 +12,6 @@ from cosmos.constants import TestBehavior
 PROJECT_ROOT_PATH=Variable.get("PROJECT_ROOT_PATH")
 
 profile_config = ProfileConfig(
-    profile_name="dbtcoredemo",
     #If you are using profiles.yml file in git use below profiles_yml_filepath
     #profiles_yml_filepath=f"{PROJECT_ROOT_PATH}/profiles.yml",
     #here we are using Airflow connection to provide profile details
