7231350e9c1350e1da446a54fedb74a2e2a9629d
ankit <ankit.devani@rackspace.com>
2023-09-06 11:31:07 +0530

    profile changes

--- dags/project.py
+++ dags/project.py
@@ -13,7 +13,6 @@ PROJECT_ROOT_PATH=Variable.get("PROJECT_ROOT_PATH")
 
 profile_config = ProfileConfig(
     profile_name="dbtcoredemo",
-    target_name="dev",
     #If you are using profiles.yml file in git use below profiles_yml_filepath
     #profiles_yml_filepath=f"{PROJECT_ROOT_PATH}/profiles.yml",
     #here we are using Airflow connection to provide profile details
